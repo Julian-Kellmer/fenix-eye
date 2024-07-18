9e46a4372bece3d265bb7a4d6b3b7fb72238c7b3
Marco <marcomussa567@gmail.com>
2024-07-18 20:56:29 -0300

    k

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.getElementById('menu-toggle');
-  const nav = document.getElementById('nav');
+  const nav = document.getElementById('nav-responsive');
 
   menuToggle.addEventListener('click', function () {
     nav.classList.toggle('show');
